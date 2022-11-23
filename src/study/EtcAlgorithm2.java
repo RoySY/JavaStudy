5e891cf78efd6aaa5bc12b5d0cc7624c1d658488
Seungyub Lee <champcom90@gmail.com>
2022-11-23 09:57:16 +0900

    2022.11.23

---
+++ src/study/EtcAlgorithm2.java
@@ -0,0 +1,35 @@
+package study;
+
+public class EtcAlgorithm2 {
+	
+	// 투 포인터(Two Pointers)
+	// 특정한 합을 가지는 부분 연속 수열 찾기
+	
+	public static int n = 5;
+	public static int m = 5;
+	public static int[] arr = {1, 2, 3, 2, 5};
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int cnt = 0;
+		int intervalSum = 0;
+		int end = 0;
+		
+		for (int start = 0; start < n; start++) {
+			
+			while (intervalSum < m && end < n) {
+				intervalSum += arr[end];
+				end++;
+			}
+			
+			if (intervalSum == m) {
+				cnt++;
+			}
+			
+			intervalSum -= arr[start];
+		}
+		
+		System.out.println(cnt);
+	}
+
+}
