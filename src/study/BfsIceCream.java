97cf386576b8f6187c68567d24d2eb7d6291ce47
Seungyub Lee <champcom90@gmail.com>
2022-11-26 09:37:11 +0900

    2022.11.26

--- src/study/BfsIceCream.java
+++ src/study/BfsIceCream.java
@@ -1,8 +1,11 @@
 package study;
 
+import java.awt.Point;
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.Scanner;
 
-public class DfsIceCream {
+public class BfsIceCream {
 	
 	// 결과적으로 이 코드는 아이스크림 재료를 부을 수 있는 칸 0이 나오면 카운트를 증가하고,
 	// 그곳의 상하좌우, 그 상하좌우의 또 상하좌우들을 전부 1로 만들어줘서 0으로부터 아이스크림이 생성될 수 있는 모든 곳을 1로 바꿔주는 코드이다.
@@ -13,23 +16,48 @@ public class DfsIceCream {
 	static int m = 0;
 	static String str = "";
 	static int[][] arr = new int[1000][1000];
+	public static int[] moveRow = {-1, 1, 0, 0};
+	public static int[] moveCol = {0, 0, -1, 1};
 	
-	public static boolean dfs(int a, int b) {
+	public static boolean bfs(int a, int b) {
 		
-		if (a < 0 || a > n - 1 || b < 0 || b > m - 1) { // 범위 벗어나는지 확인
-			return false;
+		Queue<Point> q = new LinkedList<>();
+		
+		if (arr[a][b] == 0) { // 아이스크림 재료를 부을 수 있는 칸이라면
+			
+			q.offer(new Point(a,b));
+			
+			arr[a][b] = 1; // 아이스크림 재료 붓기
+			
+			while (!q.isEmpty()) {
+				
+				Point p = q.poll();
+				
+				int row = p.x;
+				int col = p.y;
+				
+				for (int z = 0; z < 4; z++) { // 아이스크림 재료를 부은 칸의 상, 하, 좌, 우 확인하며 재료를 부을 수 있다면 같이 얼려서 아이스크림 덩어리 키우기
+					int nextRow = row + moveRow[z];
+					int nextCol = col + moveCol[z];
+					
+					// 4회 반복으로 인해 nextRow, nextCol은 아이스크림 재료를 부은 칸의 상, 하, 좌, 우 좌표가 될 것
+					
+					if (nextRow < 0 || nextRow > n - 1 || nextCol < 0 || nextCol > m - 1) { // 범위를 벗어난다면 continue
+						continue;
+					}
+					
+					if (arr[nextRow][nextCol] == 0) { // nextRow, nextCol이 재료를 부을 수 있는 칸이라면
+						q.offer(new Point(nextRow,nextCol)); // nextRow, nextCol 기준 상, 하, 좌, 우 좌표 또한 같이 얼릴 수 있는 칸인지 확인을 위해 큐에 담기
+						
+						arr[nextRow][nextCol] = 1; // 재료 붓기
+					}
+				}
+			}
+			
+			return true; // 아이스크림 재료를 부을 수 있는 칸이었으므로 true 리턴하여 result 값 1 증가되도록 하기
 		}
 		
-		if (arr[a][b] == 0) { // 아이스크림을 만들 수 있는 공간이라면
-			arr[a][b] = 1; // 아이스크림을 붓기
-			dfs(a, b + 1); // 상 칸도 만들 수 있는 공간이라면 아이스크림 붓기위해 dfs함수 실행 // 여기서 또 0이 나오면 아이스크림 붓고 상,하,좌,우 체크하여 dfs함수 실행 반복 또 반복
-			dfs(a, b - 1); // 하 칸도 만들 수 있는 공간이라면 아이스크림 붓기위해 dfs함수 실행
-			dfs(a - 1, b); // 좌 칸도 만들 수 있는 공간이라면 아이스크림 붓기위해 dfs함수 실행
-			dfs(a + 1, b); // 우 칸도 만들 수 있는 공간이라면 아이스크림 붓기위해 dfs함수 실행
-			return true; // 아이스크림을 만들었다면 카운트 증가를 위해 true 반환
-		}
-		
-		return false;
+		return false; // 아이스크림 재료를 부을 수 없는 칸이므로 false 리턴
 	}
 
 	public static void main(String[] args) {
@@ -64,7 +92,7 @@ public class DfsIceCream {
 		
 		for (int k = 0; k < n; k++) {
 			for (int z = 0; z < m; z++) {
-				if (dfs(k, z)) {
+				if (bfs(k, z)) {
 					result += 1; // 결과적으로 이 result 카운트는 총 n x m 횟수 중 true일 경우에만 증가한다.
 				}
 			}
